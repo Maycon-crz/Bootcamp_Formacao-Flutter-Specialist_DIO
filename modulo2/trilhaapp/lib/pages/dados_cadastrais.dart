aef721c0e442825d651f9aca668cbbc7123f5eec
Maycon Nascimento de Oliveira <63969109+Maycon-crz@users.noreply.github.com>
2023-01-13 20:32:01 -0300

    Formulario com validação

--- modulo2/trilhaapp/lib/pages/dados_cadastrais.dart
+++ modulo2/trilhaapp/lib/pages/dados_cadastrais.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:trilhaapp/repositories/linguagens_repository.dart';
 import 'package:trilhaapp/repositories/nivel_repository.dart';
 
 class DadosCadastrais extends StatefulWidget {
@@ -14,12 +14,29 @@ class _DadosCadastraisState extends State<DadosCadastrais> {
   final datePickerController = TextEditingController(text: "");
   DateTime? dataNascimento;
   var nivelRepository = NivelRepository();
+  var linguagensRepository = LinguagensRepository();
   var nives = [];
+  var linguagens = [];
+  var linguagensSelecionadas = [];
   var nivelSelecionado = "";
+  double salarioEscolhido = 0.0;
+  int tempoDeExperiencia = 0;
+  bool salvando = false;
+
+  List<DropdownMenuItem> returnItems(int queantidadeMaxima) {
+    var items = <DropdownMenuItem>[];
+    for (var i = 0; i <= queantidadeMaxima; i++) {
+      items.add(
+        DropdownMenuItem(value: i, child: Text(i.toString())),
+      );
+    }
+    return items;
+  }
 
   @override
   void initState() {
     nives = nivelRepository.retornaNiveis();
+    linguagens = linguagensRepository.retornaLinguagens();
     super.initState();
   }
 
@@ -36,60 +53,161 @@ class _DadosCadastraisState extends State<DadosCadastrais> {
       appBar: AppBar(
         title: const Text(''),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            returnText("Nome"),
-            TextFormField(
-              controller: nomeController,
-            ),
-            returnText("Data de Nascimento"),
-            TextFormField(
-              controller: datePickerController,
-              readOnly: true,
-              onTap: () async {
-                var data = await showDatePicker(
-                  context: context,
-                  initialDate: DateTime(2000, 1, 1),
-                  firstDate: DateTime(1900, 1, 1),
-                  lastDate: DateTime(2023, 10, 23),
-                );
-                if (data != null) {
-                  datePickerController.text = data.toString();
-                  dataNascimento = data;
-                }
-              },
-            ),
-            returnText("Nível de Experiência"),
-            Column(
-              children: nives
-                  .map(
-                    (nivel) => RadioListTile(
-                      dense: true,
-                      title: Text(nivel.toString()),
-                      selected: nivelSelecionado == nivel,
-                      value: nivel.toString(),
-                      groupValue: nivelSelecionado,
-                      onChanged: (value) {
-                        print(value);
+      body: salvando
+          ? const Center(child: CircularProgressIndicator())
+          : ListView(
+              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+              children: [
+                returnText("Nome"),
+                TextFormField(
+                  controller: nomeController,
+                ),
+                returnText("Data de Nascimento"),
+                TextFormField(
+                  controller: datePickerController,
+                  readOnly: true,
+                  onTap: () async {
+                    var data = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime(2000, 1, 1),
+                      firstDate: DateTime(1900, 1, 1),
+                      lastDate: DateTime(2023, 10, 23),
+                    );
+                    if (data != null) {
+                      datePickerController.text = data.toString();
+                      dataNascimento = data;
+                    }
+                  },
+                ),
+                returnText("Nível de Experiência"),
+                Column(
+                  children: nives
+                      .map(
+                        (nivel) => RadioListTile(
+                          dense: true,
+                          title: Text(nivel.toString()),
+                          selected: nivelSelecionado == nivel,
+                          value: nivel.toString(),
+                          groupValue: nivelSelecionado,
+                          onChanged: (value) {
+                            setState(() {
+                              nivelSelecionado = value.toString();
+                            });
+                          },
+                        ),
+                      )
+                      .toList(),
+                ),
+                returnText("Linguagens preferdas"),
+                Column(
+                  children: linguagens
+                      .map(
+                        (e) => CheckboxListTile(
+                          title: Text(e),
+                          value: linguagensSelecionadas.contains(e),
+                          onChanged: (bool? value) {
+                            if (value!) {
+                              setState(() {
+                                linguagensSelecionadas.add(e);
+                              });
+                            } else {
+                              setState(() {
+                                linguagensSelecionadas.remove(e);
+                              });
+                            }
+                          },
+                        ),
+                      )
+                      .toList(),
+                ),
+                returnText(
+                    "Pretenção salarial: R\$ ${salarioEscolhido.round()}"),
+                Slider(
+                  min: 0,
+                  max: 14000,
+                  value: salarioEscolhido,
+                  onChanged: (double value) {
+                    setState(() {
+                      salarioEscolhido = value;
+                    });
+                  },
+                ),
+                returnText("Tempo de experiência"),
+                DropdownButton(
+                    value: tempoDeExperiencia,
+                    isExpanded: true,
+                    items: returnItems(50),
+                    onChanged: (value) {
+                      setState(() {
+                        tempoDeExperiencia = int.parse(value.toString());
+                      });
+                    }),
+                TextButton(
+                    onPressed: () {
+                      salvando = false;
+                      if (nomeController.text.trim().length < 3) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text("O nome deve ser preenchido!"),
+                          ),
+                        );
+                      }
+                      if (dataNascimento == null) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text("Data de nascimento inválida"),
+                          ),
+                        );
+                      }
+                      if (nivelSelecionado.trim() == "") {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text("O nível deve ser selecionado"),
+                          ),
+                        );
+                      }
+                      if (linguagensSelecionadas.isEmpty) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text(
+                                "Deve ser selecionada ao menos 1 linguagem"),
+                          ),
+                        );
+                      }
+                      if (tempoDeExperiencia == 0) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content:
+                                Text("Deve ter ao menos 1 ano de experiência"),
+                          ),
+                        );
+                      }
+                      if (salarioEscolhido == 0) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text(
+                                "A pretenção salarial deve ser maior que 0"),
+                          ),
+                        );
+                      }
+                      setState(() {
+                        salvando = true;
+                      });
+                      Future.delayed(Duration(seconds: 3), () {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text("Dados salvos com sucesso"),
+                          ),
+                        );
                         setState(() {
-                          nivelSelecionado = value.toString();
+                          salvando = false;
                         });
-                      },
-                    ),
-                  )
-                  .toList(),
+                        Navigator.pop(context);
+                      });
+                    },
+                    child: const Text("Salvar")),
+              ],
             ),
-            TextButton(
-                onPressed: () {
-                  print(dataNascimento.toString());
-                },
-                child: const Text("Salvar")),
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ modulo2/trilhaapp/lib/repositories/linguagens_repository.dart
@@ -0,0 +1,5 @@
+class LinguagensRepository {
+  List<String> retornaLinguagens() {
+    return ["Dart", "C#", "Pyton", "Java"];
+  }
+}
